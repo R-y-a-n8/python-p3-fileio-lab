0955faea00524a2ebb7ecb5ee6c3b67a9db9467f
R-y-a-n8 <ryan.kinuthia@student.moringaschool.com>
2024-09-12 00:03:27 +0300

    fileio2

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,16 +1,16 @@
 
 
 def write_file(file_name, file_content):
-    """Writes content to a .txt file, overwriting if file exists."""
+   
     with open(file_name + ".txt", 'w') as file:
         file.write(file_content)
 
 def append_file(file_name, append_content):
-    """Appends content to a .txt file, creating it if it does not exist."""
+    
     with open(file_name + ".txt", 'a') as file:
         file.write(append_content + "\n")
 
 def read_file(file_name):
-    """Reads content from a .txt file and returns it."""
+    
     with open(file_name + ".txt", 'r') as file:
         return file.read()
